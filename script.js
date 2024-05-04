b11849c15d53ea6f8430572e9f21b748bee8779b
Reisan Lewi Damara <reisan_damara@rocketmail.com>
2024-05-04 22:35:04 +0700

    up form

--- script.js
+++ script.js
@@ -6,10 +6,16 @@ const toggleNav = () => {
   mobileNav.classList.toggle("hamburger-active");
 };
 mobileNav.addEventListener("click", () => toggleNav());
+var no = 1;
 
 function hidesoal1() {
-    var x = document.getElementById("Fisika");
-      x.style.visibility = "hidden";
-    
+  var nosoal = "soal"+no;
+  var element = document.getElementById(nosoal);
+  no = no+1;
+  var nosoal = "soal"+no;
+  var element2 = document.getElementById(nosoal);
+  
+  element.style.display = 'none';
+  element2.style.display = '';
  }
  
